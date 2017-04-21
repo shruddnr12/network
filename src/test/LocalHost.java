0b605f36221ebc22e7cef168311b18cc132c05e9
bit-user <bit-user@bit>
2017-04-21 16:30:25 +0900

    LocalHost,과제1

---
+++ src/test/LocalHost.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class LocalHost {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress inetAddress =InetAddress.getLocalHost();
+			
+			String hostAddress = inetAddress.getHostAddress();
+			System.out.println(hostAddress);
+			
+			String hostName = inetAddress.getHostName();
+			System.out.println(hostName);
+					
+			byte[] addresses = inetAddress.getAddress();//추가적인 방법
+			
+			for(int i=0; i < addresses.length; i++){
+				int address = addresses[i] & 0x000000ff;
+				System.out.print(address);
+				if(i < 3){
+					System.out.print('.');
+				}
+				
+			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/util/NsLookup.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class NsLookup {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true){
+			System.out.print(">");
+		String Hostname = scanner.next();
+		if(Hostname.equals("exit"))
+		{
+			break;    
+		}
+		
+		try {
+			
+			
+			InetAddress[] inetAddress = InetAddress.getAllByName(Hostname);
+			
+			for(int i = 0; i < inetAddress.length; i++)
+			{
+				String hostAddress = inetAddress[i].getHostAddress();
+				System.out.println(inetAddress[i].getHostName() + " : " + hostAddress);
+			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		}
+		scanner.close();
+	}
+	
+}
